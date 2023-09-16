960adaab7131d7415c73cb76e8903414abd1915f
mihailo <mihailonvojinovic@gmail.com>
2023-09-16 10:45:12 +0200

    add all endpoints to server

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('Hello World!');
-})
+app.post('/order', (req,res) => {
+    res.send('order endpoint');
+});
+
+app.get('/orderbook', (req,res) => {
+    res.send('orderbook endpoint');
+});
+
+app.get('/order/:id', (req,res) => {
+    const body = 'Order by id: ' + req.params.id;
+    res.send(body);
+});
+
+app.delete('/order/all', (req,res) => {
+    res.send('This should delete all');
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000...');
